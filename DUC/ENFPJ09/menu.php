5bb55e7eea3537b98352cacb0e3210fb65ec5cc2
chauduc <chauduc.1211@gmail.com>
2014-06-03 23:05:38 +0700

    comit backup task blog mobile

---
+++ DUC/ENFPJ09/menu.php
@@ -0,0 +1,69 @@
+
+        <div class="container">
+            <div id="jf-component-bg">
+                <div id="jf-component-trans" class="rt-hidden">
+                    <div id="rt-transition" class="rt-hidden">
+                        <div id="rt-mainbody-surround">
+                            <div id="rt-main" class="mb12">
+                                <div class="rt-container container">
+                                    <div class="rt-grid-12  row-fliud">
+                                        <div class="rt-block">
+                                            <div id="rt-mainbody">
+                                                <div class="component-content">
+                                                    <div id="Kunena" class="layout">
+                                                        <div id="jf_ku_header">
+                                                            <div class="jf_ku_header_cont">
+                                                                <div class="jf_ku_modalbtn_cont">
+                                                                    <div class="jf_ku_modalbtn jf_ku_tipsy_s" type="button" data-toggle="modal" data-target="#jf_ku_quicknav" original-title="Quick Navigation"></div>
+                                                                </div>
+                                                                <div id="jf_ku_quicknav" class="jf_ku_modal modal hide fade" tabindex="-1" role="dialog" aria-labelledby="jf_ku_quicknav_label" aria-hidden="true" style="display: none;">
+                                                                    <div class="modal-header">
+                                                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                                                        <h3 id="jf_ku_quicknav_label">Quick Navigation</h3>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        <div class="jf_kunena_quicknav">
+                                                                            <div class="moduletable">
+                                                                                <h3>Kunena QuickNav</h3>
+                                                                                <ul class="menu">
+                                                                                    <li class="item-476 opened"><a href="/home-page">Home Page</a></li>
+                                                                                    <li class="item-511 closed"><a href="/recent-posts">Recent Posts</a></li>
+                                                                                    <li class="item-512 closed"><a href="/recent-topics">Recent Topics</a></li>
+                                                                                    <li class="item-513 closed"><a href="/search">Search</a></li>
+                                                                                    <li class="item-515 closed"><a href="/user-list">User list</a></li>
+                                                                                    <li class="item-516 closed"><a href="/statistics">Statistics</a></li>
+                                                                                </ul>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="jf_ku_modal_loader" style="display: none;"></div>
+                                                                </div>
+                                                                <div id="ktop">
+                                                                    <div id="ktopmenu">
+                                                                        <div id="ktab">
+                                                                            <ul class="menu">
+                                                                                <li class="item501"><a href="/forum/index"><span>Home</span></a></li>
+                                                                                <li id="current" class="item502 active"><a href="/forum/recent"><span>Recent Topics</span></a></li>
+                                                                                <li class="item504"><a href="/forum/noreplies"><span>No Replies</span></a></li>
+                                                                                <li class="item507"><a href="/forum/help"><span>Help</span></a></li>
+                                                                                <li class="item517"><a href="/forum/statistics"><span>Statistics</span></a></li>
+                                                                            </ul>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="clear"></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="menu-main/js/jquery.tipsy.js" type="text/javascript"></script>
