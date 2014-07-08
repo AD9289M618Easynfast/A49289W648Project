b20b90a1630dba2f56340d30ca5c1d18d6e19625
zBaoAnhLe <zBaoAnhLe@gmail.com>
2014-07-08 22:40:35 +0700

    update latest

--- Html or Php/photographybygershon/template/template.php
+++ Html or Php/photographybygershon/template/template.php
@@ -1,9 +1,10 @@
 <?php
-
-$title = "";
-include 'header.php';
+$title = "Home";
+$sectionWrapper = str_replace(" ", "_", strtolower($title)) . "_content_wrapper'";
+include './template/header.php';
 ?>
+<div class="<?php echo $sectionWrapper; ?>">
+</div>
 <?php
-
 include 'template/footer.php';
 
