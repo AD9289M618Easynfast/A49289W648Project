4482f1142012464df6b565a42a287a3a0cb2cd75
chauduc <chauduc.1211@gmail.com>
2014-07-11 21:36:16 +0700

    commit add link aboutus, contact to homepage

--- Html or Php/photographybygershon/template/header.php
+++ Html or Php/photographybygershon/template/header.php
@@ -18,13 +18,13 @@
                     </a>
                     <ul class="main-nav">
                         <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
+                        <li><a href="../about-us.php">About</a></li>
                         <li><a href="#">Blog</a></li>
                         <li><a href="#">Exhibitions</a></li>
                         <li><a href="#">Portfolio</a></li>
                         <li><a href="#">Media</a></li>
                         <li><a href="#">Mailing List</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a href="../contact.php">Contact</a></li>
                     </ul>
                 </div>
                 <div class="clear"></div>
