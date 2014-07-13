50ad19ff34243025e94f6be43627fe1a166f944c
chauduc <chauduc.1211@gmail.com>
2014-07-13 12:29:15 +0700

    commit fixbug mailinglist

--- Html or Php/photographybygershon/mailinglist.php
+++ Html or Php/photographybygershon/mailinglist.php
@@ -6,15 +6,13 @@ include 'template/header.php';
 
 <div class="<?php echo $sectionWrapper ?>">
     <div class="left-content-mailing-wrapper">
-        <img src="images/mailinglist_02.png" alt="about-gershon-kelman" width="376" height="66" class="left-content-title" />
+        <p class="title">JOIN THE MAILING LIST</p>
         <div class="mailing-info">
             <p>Enter your email below, to join the mailing list, and let us know how you heard about Gershon Kelman.</p>
             <p><iframe src="http://www.foxyform.com/form.php?id=438992&sec_hash=23840be752f" width="350px" height="333px" frameborder="0"></iframe> </p>
         </div>
     </div>
-    <div class="right-content-mailing-wrapper">
-        <img src="images/mailinglist_03.png" alt="about-gershon-kelman" width="475" height="526" class="right-content" />
-    </div>
+    <img src="images/mailinglist_03.png" alt="about-gershon-kelman" width="475" height="526" class="right-image-content" />
     <div class="clear"></div>
 </div>
 
