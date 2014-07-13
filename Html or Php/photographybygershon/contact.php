0dbf76d0a893163ca0ca78c9195392a8b054a7c3
chauduc <chauduc.1211@gmail.com>
2014-07-13 12:12:32 +0700

    commit fixbug contact.php

--- Html or Php/photographybygershon/contact.php
+++ Html or Php/photographybygershon/contact.php
@@ -7,16 +7,26 @@ include 'template/header.php';
 
 <div class="<?php echo $sectionWrapper ?>">
     <div class="left-content-contact-wrapper">
-        <img src="images/contact_02.png" alt="about-gershon-kelman" width="313" height="115" class="left-content-title" />
+        <div class="contact-title">
+            <p>CONTACT GERSHON</p>
+            <ul class="social-nav">
+                <li><a href=""><img src="images/icon/icon-facebook.jpg" /></a></li>
+                <li><a href=""><img src="images/icon/icon-tumblr.jpg" /></a></li>
+                <li><a href=""><img src="images/icon/icon-twitter.jpg" /></a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
         <div class="contact-info">
             <p><span class="phone-contact">Phone:</span> 661-713-9386</p>
             <p><span class="email-contact">E-Mail:</span><a href="mailto:photographybygershon@gmail.com">photographybygershon@gmail.com</a></p>
         </div>
     </div>
-    <div class="right-content-about-wrapper">
-        <img src="images/contact_03.png" alt="about-gershon-kelman" width="434" height="487" class="right-content" />
-    </div>
+    <img src="images/contact_03.png" alt="about-gershon-kelman" width="434" height="487" class="right-image-content" />
     <div class="clear"></div>
 </div>
+
+<?php
+
+include 'template/footer.php';
 
 
