f7c8791ec745bd28fb85ce7d753a43bb223a1396
chauduc <chauduc.1211@gmail.com>
2014-07-13 14:25:36 +0700

    commit exhibitions.php

---
+++ Html or Php/photographybygershon/exhibitions.php
@@ -0,0 +1,159 @@
+<?php
+$title = "Exhibitions";
+$sectionWrapper = str_replace(" ", "-", strtolower($title)) . "-content-wrapper";
+include 'template/header.php';
+?>
+
+<div class="<?php echo $sectionWrapper ?>">
+    <div class="upcoming-exhibitions">
+        <p class="upcoming-title">UPCOMING EXHIBITIONS</p>
+        <ul>
+            <li>
+                <img src="images/miami_exhibition.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">ART FUSION GALLERY (MIAMI)</p>
+                    <p class="content">
+                        "The Essence of Triumph and Color"<br />
+                        <span>July - September 2014</span> 
+                        <br />
+                        <br />
+                        3550 N Miami Ave,<br /> 
+                        Miami, FL 33127
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+        </ul>
+    </div>
+    <div class="previous-exhibitions">
+        <p class="previous-title">PREVIOUS EXHIBITIONS</p>
+        <ul>
+            <li>
+                <img src="images/salon_electron.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">AMSTERDAM WHITNEY GALLERY, NYC</p>
+                    <p class="content">
+                        <span>March 27th 2014</span>
+                        <br />
+                        <br />
+                        511 W 25th St #406,<br />
+                        New York, NY 10001
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/salon_electron.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">ELECTRON SALON EXHIBIT</p>
+                    <p class="content">
+                        <span>June 13th (Reception 7-9pm) - July 6th</span>
+                        <br />
+                        <br />
+                        Los Angeles Center for Digital Art<br />
+                        102 West 5th Street (Corner of Spring & Main)
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/four_seasons.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">FOUR SEASONS HOTEL</p>
+                    <p class="content">
+                        <span>May 1st 2013</span>
+                        <br />
+                        <br />
+                        9500 Wilshire Blvd. <br />
+                        Beverly Hills, CA 90212
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/bruce_lurie.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">BRUCE LURIE GALLERY</p>
+                    <p class="content">
+                        <span>December 15th</span>
+                        <br />
+                        <br />
+                        2736 La Cienega Blvd.<br />
+                        Culver City, CA
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/petra.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">PETRA GALLERY</p>
+                    <p class="content">
+                        <span>November 20th - December 20th</span>
+                        <br />
+                        <br />
+                        1151 South Robertson Blvd.<br />
+                        Los Angeles, CA 90035
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/BH_artshow.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">BEVERLY HILLS ART SHOW</p>
+                    <p class="content">
+                        <span>October 20th - 21st</span>
+                        <br />
+                        <br />
+                        Beverly Gardens<br />
+                        9350 North Santa Monica Blvd. Beverly Hills, CA
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/zimmerman.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">COMMUNITY SERVICE CENTER OPENING ASSEMBLYMAN:<br />
+                        MITCHELL ENGLANDER OF THE 12th DISTRICT</p>
+                    <p class="content">
+                        <br />
+                        <br />
+                        <span>July 23rd</span><br />
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/pasadena.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">PASADENA ARTWALK</p>
+                    <p class="content">
+                        <br />
+                        <br />
+                        <span>October 20th - 21st</span>    <br />
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <img src="images/terrell.png" width="200" height="155" class="left-image" />
+                <div class="right-content">
+                    <p class="title">ATERRELL MOORE GALLERY</p>
+                    <p class="content">
+                        <br />
+                        <br />
+                        October 15th 2011 - February 23rd 2012<br />
+                    </p>
+                </div>
+                <div class="clear"></div>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<?php
+include 'template/footer.php';
+
+
